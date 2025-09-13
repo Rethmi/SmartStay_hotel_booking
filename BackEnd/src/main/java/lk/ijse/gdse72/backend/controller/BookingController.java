bdb2a71f1fd4bbf2f9f85a05b17fe0645bbb5782
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2025-09-13 12:20:22 +0530

    update booking

--- BackEnd/src/main/java/lk/ijse/gdse72/backend/controller/BookingController.java
+++ BackEnd/src/main/java/lk/ijse/gdse72/backend/controller/BookingController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/bookings")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://127.0.0.1:5501")
 public class BookingController {
 
     private final BookingService bookingService;
